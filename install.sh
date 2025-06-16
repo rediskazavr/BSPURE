4f0a346a7b30a3c42fceadfcf2d9b29ee8de8e29
MatveySDK <matveywindows60@gmail.com>
2025-06-16 21:03:13 +0300

    Edit

--- install.sh
+++ install.sh
@@ -10,36 +10,38 @@
 # Update system
 sudo pacman -Syu
 
-# install pacakges
+# Install packages
 sudo pacman -S xorg xorg-xinit bspwm sxhkd alacritty feh rofi git base-devel neovim fastfetch cava cmatrix firefox thunar tgpt zsh --noconfirm
 
-# create folders for configs
-sudo mkdir ~/.config
+# Create folders for configs (skip if exists)
+mkdir -p ~/.config
 
-# copy dotfiles
-sudo cp ~/BSPURE/dotfiles/wallpaper.png ~/.config/
-sudo cp ~/BSPURE/dotfiles/alacritty/ ~/.config/
-sudo cp ~/BSPURE/dotfiles/bspwm/ ~/.config/
-sudo cp ~/BSPURE/dotfiles/sxhkd/ ~/.config/
-sudo cp ~/BSPURE/dotfiles/alacritty/ ~/.config/
+# Copy dotfiles (with -r for directories)
+cp ~/BSPURE/dotfiles/wallpaper.png ~/.config/
+cp -r ~/BSPURE/dotfiles/alacritty/ ~/.config/
+cp -r ~/BSPURE/dotfiles/bspwm/ ~/.config/
+cp -r ~/BSPURE/dotfiles/sxhkd/ ~/.config/
 
-# Install zsh
-sudo chsh -s $(which zsh)
-sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-sudo echo "export ZSH="$HOME/.oh-my-zsh"
+# Install zsh and Oh My Zsh (for current user, not root)
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Create .zshrc correctly
+cat <<EOF > ~/.zshrc
+export ZSH="\$HOME/.oh-my-zsh"
 ZSH_THEME="bira"
-source $ZSH/oh-my-zsh.sh" > ~/.zshrc
+source \$ZSH/oh-my-zsh.sh
+EOF
 
-# Install yay
-sudo cd ~/BSPURE/packages/yay/
-sudo makepkg -si
+# Install yay (as normal user)
+cd ~/BSPURE/packages/yay/
+makepkg -si
 
-# Install Fonts
-sudo yay -Syu
-sudo yay -S pxplus-ibm-vga8
-sudo fc-cache -fv
-sudo clear
+# Install Fonts (yay doesn't need sudo)
+yay -S pxplus-ibm-vga8
+fc-cache -fv
+clear
 
 # Reboot system
 sudo reboot
